c68afc2d346a3b3f965c780e324ca3f6d0e7732a
Shreyas Rajesh <shreyasrajesh0917@gmail.com>
2025-01-11 13:31:39 -0800

    Fix

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,7 +1,3 @@
 import { addUserToFirestore } from "./auth/addUserToFirestore.js";
 
 export const addUserToFirestore_CF = addUserToFirestore;
-
-const admin = require('firebase-admin');
-admin.initializeApp();
-const db = admin.firestore();
